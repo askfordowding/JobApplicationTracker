60631ae9229d873db85f3e48fae7f67750833e2a
askfordowding <42510070+askfordowding@users.noreply.github.com>
2026-08-24 20:21:23 +0100

    security: restrict browser origins to loopback

---
+++ JobApplicationTracker/JobApplicationService.API/Security/SecurityPolicy.cs
@@ -0,0 +1,22 @@
+using System.Net;
+
+namespace JobApplicationService.API.Security;
+
+public static class SecurityPolicy
+{
+    public static bool IsAllowedLocalOrigin(string? origin)
+    {
+        if (!Uri.TryCreate(origin, UriKind.Absolute, out var uri) ||
+            (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps))
+        {
+            return false;
+        }
+
+        if (string.Equals(uri.Host, "localhost", StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+
+        return IPAddress.TryParse(uri.Host, out var address) && IPAddress.IsLoopback(address);
+    }
+}
