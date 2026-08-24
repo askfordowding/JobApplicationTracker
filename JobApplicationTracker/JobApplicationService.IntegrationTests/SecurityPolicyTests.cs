66a266dfbae86a138c93c440e5f20634a5f4e103
askfordowding <42510070+askfordowding@users.noreply.github.com>
2026-08-24 20:21:52 +0100

    test: cover local-only API access policy

--- JobApplicationTracker/JobApplicationService.IntegrationTests/SecurityPolicyTests.cs
+++ JobApplicationTracker/JobApplicationService.IntegrationTests/SecurityPolicyTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using JobApplicationService.API.Security;
 
 namespace JobApplicationService.IntegrationTests;
@@ -22,5 +23,23 @@ public sealed class SecurityPolicyTests
     public void IsAllowedLocalOrigin_RejectsNonLoopbackOrigins(string origin)
     {
         Assert.False(SecurityPolicy.IsAllowedLocalOrigin(origin));
+    }
+
+    [Theory]
+    [InlineData("127.0.0.1")]
+    [InlineData("::1")]
+    [InlineData("::ffff:127.0.0.1")]
+    public void IsAllowedRemoteAddress_AllowsLoopback(string address)
+    {
+        Assert.True(SecurityPolicy.IsAllowedRemoteAddress(IPAddress.Parse(address)));
+    }
+
+    [Theory]
+    [InlineData("192.168.1.25")]
+    [InlineData("10.0.0.10")]
+    [InlineData("8.8.8.8")]
+    public void IsAllowedRemoteAddress_RejectsNonLoopback(string address)
+    {
+        Assert.False(SecurityPolicy.IsAllowedRemoteAddress(IPAddress.Parse(address)));
     }
 }
